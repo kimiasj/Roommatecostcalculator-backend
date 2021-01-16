c7b63144eab35b99ad5e0f70439a5bb58de8a88a
Kimia Sajjadi <67805546+kimiasj@users.noreply.github.com>
2021-01-16 02:40:47 -0330

    loadCost

--- RoommateCostCalculator/src/main/java/com/RoommateCostCalculator/RoommateCostCalculator/Models/Cost.java
+++ RoommateCostCalculator/src/main/java/com/RoommateCostCalculator/RoommateCostCalculator/Models/Cost.java
@@ -49,6 +49,26 @@ public class Cost {
             this.id = generatedKey;
         }con.close();
     }
+    public static void loadCosts() throws SQLException , ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con= DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/roommatescostcalculator","root","123456");
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery("Select * from costs ");
+
+        while(rs.next()){
+            for (User i:newuser
+            ) {
+                if (i.id == rs.getInt(5)){
+                    Cost dbcost = new Cost(rs.getInt(1),rs.getDouble(2),Category.valueOf(rs.getString(3)),i,rs.getDate(4));
+                    i.addCost(dbcost);
+                }
+
+            }
+
+
+        }
+    }
 
     public void delete() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.jdbc.Driver");
